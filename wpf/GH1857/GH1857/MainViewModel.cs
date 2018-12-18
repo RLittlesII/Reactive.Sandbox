452b11dfdb66d07ae70ee7edc8e210802f9ed580
RLittlesII <rodney.littles@gmail.com>
2018-12-18 06:52:51 -0600

    GH-1857

---
+++ wpf/GH1857/GH1857/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace GH1857
+{
+    public class MainViewModel : ReactiveObject, IDisposable
+    {
+        private Subject<(string title, string text)> _onMessage = new Subject<(string title, string text)>();
+        private CompositeDisposable disposable = new CompositeDisposable();
+
+        public ReactiveCommand<Unit, string> CreateProjectCommand { get; set; }
+
+        public IObservable<(string title, string text)> OnMessage => _onMessage.AsObservable();
+
+        public MainViewModel()
+        {
+            CreateProjectCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                await Task.Delay(1000);
+                return "SomeValue";
+            });
+
+            CreateProjectCommand
+                .Where(r => r != null)
+                .Select(r =>
+                {
+                    //breakpoint here
+                    return ("Успешно", $"Проект '{r}' успешно создан.");
+                })
+                .Subscribe(_onMessage)
+                .DisposeWith(disposable);
+        }
+
+        public void Dispose()
+        {
+            Interlocked.Exchange(ref disposable, null)?.Dispose();
+        }
+    }
+}
---
+++ wpf/GH1857/GH1857/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace GH1857
+{
+    public partial class MainWindow : ReactiveWindow<MainViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+            this.WhenActivated(d =>
+            {
+                d(this.BindCommand(ViewModel, x => x.CreateProjectCommand, v => v.Create));
+            });
+        }
+    }
+}
