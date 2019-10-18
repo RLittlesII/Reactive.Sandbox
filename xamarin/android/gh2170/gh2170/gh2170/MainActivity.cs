59da01d6c3322d1a59fb765961e12dfe7a14d86d
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-10-17 22:00:48 -0500

    GH-2170

---
+++ xamarin/android/gh2170/gh2170/gh2170/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using ReactiveUI;
+
+namespace gh2170
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ReactiveActivity<MainViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            ViewModel = new MainViewModel();
+
+            this.WireUpControls();
+
+            this.Bind(ViewModel, vm => vm.Text, view => view.EditText.Text);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public EditText EditText { get; set; }
+    }
+
+    public class MainViewModel : ReactiveObject
+    {
+        public string Text { get; set; }
+    }
+}
