b43feb7a3d3d63e6b3ca98de20052516c35f83e3
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-07-01 22:00:14 -0500

    sample: Added Search Function

--- xamarin/forms/DynamicList/DynamicList/Crud/ListViewModel.cs
+++ xamarin/forms/DynamicList/DynamicList/Crud/ListViewModel.cs
@@ -21,13 +21,30 @@ namespace DynamicList.Crud
         {
             _drinkDataService = drinkDataService;
 
-            this.WhenAnyValue(x => x.SearchText);
+            Func<ItemViewModel, bool> search(string searchTerm) =>
+                viewModel =>
+                {
+                    if (string.IsNullOrEmpty(searchTerm))
+                    {
+                        return true;
+                    }
+
+                    var lower = searchTerm.ToLower();
+                    return viewModel.Title.ToLower().Contains(lower) || (viewModel.Description?.ToLower().Contains(lower) ?? false);
+                };
+
+            var searchChanged =
+                this.WhenAnyValue(x => x.SearchText)
+                    .Throttle(TimeSpan.FromMilliseconds(800), RxApp.TaskpoolScheduler)
+                    .DistinctUntilChanged()
+                    .Select(search);
 
             _drinkDataService
                 .ChangeSet
-                .Transform(x => new ItemViewModel { Id = x.Id, Title = x.Title, Type = x.Type })
+                .Transform(x => new ItemViewModel { Id = x.Id, Title = x.Title, Type = x.Type, Description = x.Description})
                 .AutoRefresh(x => x.Id)
                 .DeferUntilLoaded()
+                .Filter(searchChanged)
                 .Sort(SortExpressionComparer<ItemViewModel>.Descending(x => x.Type).ThenByAscending(x => x.Id))
                 .ObserveOn(RxApp.MainThreadScheduler)
                 .Bind(out _items)
@@ -65,16 +82,25 @@ namespace DynamicList.Crud
         }
 
         private IObservable<Unit> ExecuteAdd(EventArgs args) =>
-            Observable.Create<Unit>(observer => Interactions.AddItem.Handle(Unit.Default).ObserveOn(RxApp.MainThreadScheduler).Subscribe(observer).DisposeWith(ViewModelSubscriptions));
+            Observable
+                .Create<Unit>(observer =>
+                    Interactions
+                        .AddItem
+                        .Handle(Unit.Default)
+                        .ObserveOn(RxApp.MainThreadScheduler)
+                        .Subscribe(observer)
+                        .DisposeWith(ViewModelSubscriptions));
 
-        private IObservable<Unit> ExecuteRefresh(EventArgs args) => Observable.Create<Unit>(observer =>
-            _drinkDataService
-                .Read()
-                .Select(x => Unit.Default)
-                .Delay(TimeSpan.FromSeconds(2), RxApp.TaskpoolScheduler)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .Subscribe(observer)
-                .DisposeWith(ViewModelSubscriptions));
+        private IObservable<Unit> ExecuteRefresh(EventArgs args) =>
+            Observable
+                .Create<Unit>(observer =>
+                    _drinkDataService
+                        .Read()
+                        .Select(x => Unit.Default)
+                        .Delay(TimeSpan.FromSeconds(2), RxApp.TaskpoolScheduler)
+                        .ObserveOn(RxApp.MainThreadScheduler)
+                        .Subscribe(observer)
+                        .DisposeWith(ViewModelSubscriptions));
 
         private IObservable<Unit> ExecuteRemove(ItemViewModel item) => _drinkDataService.Delete(item.Id);
     }
