7f56d03930286e558615201dbbfed72017531026
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-07-29 21:23:59 -0500

    sample

--- xamarin/forms/WhenAny/WhenAny/MainViewModel.cs
+++ xamarin/forms/WhenAny/WhenAny/MainViewModel.cs
@@ -3,11 +3,14 @@ using Sextant;
 
 namespace WhenAny
 {
-    public class MainViewModel : ReactiveObject
+    public class MainViewModel : ReactiveObject, IViewModel
     {
         public MainViewModel()
         {
+            this.WhenAny()
             
         }
+
+        public string Id { get; }
     }
 }
